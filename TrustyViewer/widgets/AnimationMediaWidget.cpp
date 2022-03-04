846fb5e0e167914fc402ee9e0092e5f9617d56ca
realn <realnoname@coderulers.info>
2022-03-04 10:26:54 +0100

    Added position slider to animation widget.

--- TrustyViewer/widgets/AnimationMediaWidget.cpp
+++ TrustyViewer/widgets/AnimationMediaWidget.cpp
@@ -1,22 +1,32 @@
 
 #include <QBoxLayout>
 
+#include "VideoButtonsWidget.h"
 #include "AnimationMediaWidget.h"
 
 namespace realn {
-  AnimationMediaWidget::AnimationMediaWidget()
-  {
+  AnimationMediaWidget::AnimationMediaWidget() {
     animationPlayer = new QLabel();
-    animationPlayer->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    animationPlayer->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::MinimumExpanding);
     animationPlayer->setAlignment(Qt::AlignCenter);
 
-    auto layout = new QHBoxLayout();
+    videoButtonsWidget = new VideoButtonsWidget();
+    videoButtonsWidget->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Minimum);
+
+    connect(videoButtonsWidget, &VideoButtonsWidget::sliderPositionChanged, this, &AnimationMediaWidget::onSliderPositionChanged);
+    connect(videoButtonsWidget, &VideoButtonsWidget::sliderPositionPressed, this, &AnimationMediaWidget::onSliderPositionPressed);
+    connect(videoButtonsWidget, &VideoButtonsWidget::sliderPositionReleased, this, &AnimationMediaWidget::onSliderPositionReleased);
+    connect(videoButtonsWidget, &VideoButtonsWidget::videoPositionTimeout, this, &AnimationMediaWidget::updateSliderPosition);
+
+    auto layout = new QVBoxLayout();
     layout->addWidget(animationPlayer);
+    layout->addWidget(videoButtonsWidget);
     setLayout(layout);
   }
 
-  bool AnimationMediaWidget::loadMedia(MediaItem::ptr_t mediaItem, std::shared_ptr<ExtPlugin> plugin)
-  {
+  AnimationMediaWidget::~AnimationMediaWidget() = default;
+
+  bool AnimationMediaWidget::loadMedia(MediaItem::ptr_t mediaItem, std::shared_ptr<ExtPlugin> plugin) {
     if (!mediaItem || !plugin)
       return false;
 
@@ -32,11 +42,39 @@ namespace realn {
     return true;
   }
 
-  void AnimationMediaWidget::clearMedia()
-  {
+  void AnimationMediaWidget::clearMedia() {
     if (movie)
       movie->stop();
     animationPlayer->setMovie(nullptr);
     movie.reset();
+  }
+
+  void AnimationMediaWidget::onSliderPositionChanged() {
+    movie->jumpToFrame(getSliderPositionForAnimation());
+  }
+
+  void AnimationMediaWidget::onSliderPositionPressed() {
+    movie->setPaused(true);
+  }
+
+  void AnimationMediaWidget::onSliderPositionReleased() {
+    movie->setPaused(false);
+  }
+
+  void AnimationMediaWidget::updateSliderPosition() {
+    videoButtonsWidget->setVideoPosition(getAnimationPositionForSlider());
+  }
+
+  int AnimationMediaWidget::getSliderPositionForAnimation() const {
+    if(!movie)
+      return 0;
+    auto pos = videoButtonsWidget->getVideoPosition() * movie->frameCount();
+    return static_cast<int>(pos);
+  }
+
+  float AnimationMediaWidget::getAnimationPositionForSlider() const {
+    if (!movie)
+      return 0.0f;
+    return static_cast<float>(movie->currentFrameNumber()) / movie->frameCount();
   }
 }
--- TrustyViewer/widgets/AnimationMediaWidget.h
+++ TrustyViewer/widgets/AnimationMediaWidget.h
@@ -8,18 +8,30 @@
 #include "BaseMediaContentPlayerWidget.h"
 
 namespace realn {
+  class VideoButtonsWidget;
+
   class AnimationMediaWidget : public BaseMediaContentPlayerWidget {
     Q_OBJECT;
   public:
     AnimationMediaWidget();
+    ~AnimationMediaWidget();
 
     // Inherited via BaseMediaContentPlayerWidget
     virtual bool loadMedia(MediaItem::ptr_t mediaItem, std::shared_ptr<ExtPlugin> plugin) override;
     virtual void clearMedia() override;
 
-  private:
-    QPointer<QLabel> animationPlayer;
+  private slots:
+    void onSliderPositionChanged();
+    void onSliderPositionPressed();
+    void onSliderPositionReleased();
+    void updateSliderPosition();
 
+  private:
+    int getSliderPositionForAnimation() const;
+    float getAnimationPositionForSlider() const;
+
+    QPointer<QLabel> animationPlayer;
+    QPointer<VideoButtonsWidget> videoButtonsWidget;
     std::unique_ptr<QMovie> movie;
   };
 }
